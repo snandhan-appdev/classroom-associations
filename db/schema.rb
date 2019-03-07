71bd3aec47db5ade3f86b3b285db18b9409bad01
Tosh Nandhan <snandhan@chicagobooth.edu>
2019-03-07 06:12:52 +0000

    75% complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190228215329) do
+ActiveRecord::Schema.define(version: 20190307045730) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -52,6 +52,13 @@ ActiveRecord::Schema.define(version: 20190228215329) do
 
   create_table "departments", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "enrollments", force: :cascade do |t|
+    t.integer "course_id"
+    t.integer "student_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
